58e6e78c26fbb111901bd344e09711d48130fce8
Santiago <sancastillog@gmail.com>
2021-07-06 20:29:49 -0500

    update prompt

--- height.rb
+++ height.rb
@@ -2,5 +2,7 @@ require './HeightMatcher.rb'
 include HeightMatcher
 
 players = fetch_players
+puts "Welcome to NBA height matcher"
+puts "To find players given their height please input the height in inches"
 height = ask_height
 puts match_height_to_names(players, height)
